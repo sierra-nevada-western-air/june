75ed79221c4a40896a875be4e9947fab8c57afb3
Michael Bradvica <mjbradvica@gmail.com>
2023-06-24 20:33:45 -0400

    updated viewports

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,5 +4,7 @@ export default defineConfig({
   e2e: {
     specPattern: "cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}",
     baseUrl: "http://localhost:5173",
+    viewportHeight: 1280,
+    viewportWidth: 1024,
   },
 });
